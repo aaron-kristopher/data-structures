1f2fbb858a2d5d6bfea8d9ab390e1f6016f3ebe6
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-01-15 20:06:29 +0800

    Create Node class inside LinkedList class

--- code/src/dataStructures/LinkedList.java
+++ code/src/dataStructures/LinkedList.java
@@ -2,4 +2,19 @@ package dataStructures;
 
 public class LinkedList {
 
+    // TODO addFirst
+    // TODO addLast
+    // TODO deleteFirst
+    // TODO deleteLast
+    // TODO contains
+    // TODO indexOf
+
+    private class Node {
+        int value;
+        Node next;
+
+        Node(int item) {
+            value = item;
+        }
+    }
 }
