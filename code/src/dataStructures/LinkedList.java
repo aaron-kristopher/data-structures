e0ad3ee37ec2388ba59754819eae8c79dae3f4f4
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-01-15 20:18:27 +0800

    Implement addFirst and print method in LinkedList

--- code/src/dataStructures/LinkedList.java
+++ code/src/dataStructures/LinkedList.java
@@ -9,12 +9,37 @@ public class LinkedList {
     // TODO contains
     // TODO indexOf
 
+    private Node first;
+    private Node last;
+    private int count = 0;
+
     private class Node {
         int value;
         Node next;
 
         Node(int item) {
             value = item;
+        }
+    }
+
+    public void addFirst(int item) {
+        Node node = new Node(item);
+        if (first == null)
+            first = node;
+        else {
+            var oldFirst = first;
+            node.next = oldFirst;
+            first = node;
+        }
+
+        count++;
+    }
+
+    public void print() {
+        var node = first;
+        for (int i = 0; i < count; i++) {
+            System.out.println(node.value);
+            node = node.next;
         }
     }
 }
