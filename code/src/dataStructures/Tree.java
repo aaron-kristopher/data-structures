0b342a80126b7d66d3faaafe454c6795c6e91de9
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-01-26 13:53:00 +0800

    Change 2 space indent to 4 spaces

--- code/src/dataStructures/Tree.java
+++ code/src/dataStructures/Tree.java
@@ -1,84 +1,85 @@
 package dataStructures;
 
 public class Tree {
-  public static void main(String[] args) {
-    Tree tree = new Tree();
-    tree.insert(7);
-    tree.insert(6);
-    tree.insert(4);
-    tree.insert(8);
-    tree.insert(1);
-    tree.insert(9);
-    tree.insert(10);
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        tree.insert(7);
+        tree.insert(6);
+        tree.insert(4);
+        tree.insert(8);
+        tree.insert(1);
+        tree.insert(9);
+        tree.insert(10);
 
-    /*
-     * TREE VISUALIZATION
-     * ___________7
-     *
-     * _____4___________9
-     *
-     * __1_____6_____8_____10
-     *
-     *
-     */
-    System.out.println(tree.find(4));
-    System.out.println(tree.find(10));
-  }
-
-  private class Node {
-    public int value;
-    public Node leftChild;
-    public Node rightChild;
-
-    Node(int value) {
-      this.value = value;
+        /*
+         * TREE VISUALIZATION
+         * ___________7
+         *
+         * _____4___________9
+         *
+         * __1_____6_____8_____10
+         *
+         *
+         */
+        System.out.println(tree.find(4));
+        System.out.println(tree.find(10));
     }
 
-    @Override
-    public String toString() {
-      return "Node=" + value;
-    }
-  }
+    private class Node {
+        public int value;
+        public Node leftChild;
+        public Node rightChild;
 
-  private Node root;
-
-  public void insert(int value) {
-    Node node = new Node(value);
-    Node current = root;
-
-    if (current == null) {
-      root = node;
-      return;
-    }
-
-    while (true) {
-      if (current.value < value) {
-        if (current.rightChild == null) {
-          current.rightChild = node;
-          break;
+        Node(int value) {
+            this.value = value;
         }
-        current = current.rightChild;
-      } else {
-        if (current.leftChild == null) {
-          current.leftChild = node;
-          break;
+
+        @Override
+        public String toString() {
+            return "Node=" + value;
         }
-        current = current.leftChild;
-      }
     }
-  }
 
-  public boolean find(int value) {
-    Node current = root;
+    private Node root;
 
-    while (current != null) {
-      if (current.value < value)
-        current = current.rightChild;
-      else if (current.value > value)
-        current = current.leftChild;
-      else
-        return true;
+    public void insert(int value) {
+        Node node = new Node(value);
+        Node current = root;
+
+        if (current == null) {
+            root = node;
+            return;
+        }
+
+        while (true) {
+            if (current.value < value) {
+                if (current.rightChild == null) {
+                    current.rightChild = node;
+                    break;
+                }
+                current = current.rightChild;
+            } else {
+                if (current.leftChild == null) {
+                    current.leftChild = node;
+                    break;
+                }
+                current = current.leftChild;
+            }
+        }
     }
-    return false;
-  }
+
+    public boolean find(int value) {
+        Node current = root;
+
+        while (current != null) {
+            if (current.value < value)
+                current = current.rightChild;
+            else if (current.value > value)
+                current = current.leftChild;
+            else
+                return true;
+        }
+        return false;
+    }
+
 }
